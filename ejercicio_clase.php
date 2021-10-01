48f195d0b436542f30a46d62cee03f0f3dcf3f76
JuanJesuss <90684570+JuanJesuss@users.noreply.github.com>
2021-10-01 13:29:25 +0200

    Add files via upload

---
+++ ejercicio_clase.php
@@ -0,0 +1,58 @@
+<HTML>
+<HEAD>
+<TITLE>EJ1-Conversion IP Decimal a Binario</TITLE>
+</HEAD>
+
+<BODY>
+
+<?php
+
+$bolsa = array (
+    array("Acciona", "143,10", "-0,07%", "-0,10,", "3.856.364,50", "0,00", "23,35", "2,79%", "12:07"),
+    array("Acerinox", 0, 0, 0, 0, 0, 0, 0, 0),
+    array("ACS", 0, 0, 0, 0, 0, 0, 0, 0),
+    array("AENA", 0, 0, 0, 0, 0, 0, 0, 0),
+    array("ALMIRALL", 0, 0, 0, 0, 0, 0, 0, 0),
+    array("AMADEUS", 0, 0, 0, 0, 0, 0, 0, 0),
+    array("ARCELORMITTAL", 0, 0, 0, 0, 0, 0, 0, 0),
+    array("BANKINTER", 0, 0, 0, 0, 0, 0, 0, 0),
+    array("BBVA", 0, 0, 0, 0, 0, 0, 0, 0),
+    array("CAIXABANK", 0, 0, 0, 0, 0, 0, 0, 0),
+    array("CELLNEX TELECOM", 0, 0, 0, 0, 0, 0, 0, 0),
+    array("CIE AUTOMOTIVE", 0, 0, 0, 0, 0, 0, 0, 0),
+    array("ENAGAS", 0, 0, 0, 0, 0, 0, 0, 0),
+    array("ENDESA", 0, 0, 0, 0, 0, 0, 0, 0),
+    array("FERROVIAL", 0, 0, 0, 0, 0, 0, 0, 0),
+    array("FLUIDRA", 0, 0, 0, 0, 0, 0, 0, 0),
+    array("GRIFOLS", 0, 0, 0, 0, 0, 0, 0, 0),
+    array("IAG(IBERIA)", 0, 0, 0, 0, 0, 0, 0, 0),
+    array("IBERDROLA", 0, 0, 0, 0, 0, 0, 0, 0),
+    array("INDITEX", 0, 0, 0, 0, 0, 0, 0, 0),
+    array("INDRA", 0, 0, 0, 0, 0, 0, 0, 0),
+    array("INM. COLONIAL", 0, 0, 0, 0, 0, 0, 0, 0),
+    array("MAPFRE", 0, 0, 0, 0, 0, 0, 0, 0),
+    array("MELIA HOTELS INTL", 0, 0, 0, 0, 0, 0, 0, 0),
+    array("MERLIN PROP.", 0, 0, 0, 0, 0, 0, 0, 0),
+    array("NATURGY", 0, 0, 0, 0, 0, 0, 0, 0),
+    array("PHARMA MAR R", 0, 0, 0, 0, 0, 0, 0, 0),
+    array("REE", 0, 0, 0, 0, 0, 0, 0, 0),
+    array("REPSOL", 0, 0, 0, 0, 0, 0, 0, 0),
+    array("SABADELL", 0, 0, 0, 0, 0, 0, 0, 0),
+    array("SANTANDER", 0, 0, 0, 0, 0, 0, 0, 0),
+    array("SIEMENS GAMESA", 0, 0, 0, 0, 0, 0, 0, 0),
+    array("SOLARIA ENERGIA", 0, 0, 0, 0, 0, 0, 0, 0),
+    array("TELEFONICA", 0, 0, 0, 0, 0, 0, 0, 0),
+    array("VISCOFAN", 0, 0, 0, 0, 0, 0, 0, 0));
+
+echo $bolsa[0][0]." ".$bolsa[0][1]." ".$bolsa[0][2]." ".$bolsa[0][3]." ".$bolsa[0][4]." ".$bolsa[0][5]." ".$bolsa[0][6]." ".$bolsa[0][7]." ".$bolsa[0][8]."<br>";
+echo $bolsa[1][0]." ".$bolsa[1][1]." ".$bolsa[1][2]." ".$bolsa[1][3]." ".$bolsa[1][4]." ".$bolsa[1][5]." ".$bolsa[1][6]." ".$bolsa[1][7]." ".$bolsa[1][8]."<br>";
+echo $bolsa[2][0]." ".$bolsa[2][1]." ".$bolsa[2][2]." ".$bolsa[2][3]." ".$bolsa[2][4]." ".$bolsa[2][5]." ".$bolsa[2][6]." ".$bolsa[2][7]." ".$bolsa[2][8]."<br>";
+echo $bolsa[3][0]." ".$bolsa[3][1]." ".$bolsa[3][2]." ".$bolsa[3][3]." ".$bolsa[3][4]." ".$bolsa[3][5]." ".$bolsa[3][6]." ".$bolsa[3][7]." ".$bolsa[3][8]."<br>";
+echo $bolsa[4][0]." ".$bolsa[4][1]." ".$bolsa[4][2]." ".$bolsa[4][3]." ".$bolsa[4][4]." ".$bolsa[4][5]." ".$bolsa[4][6]." ".$bolsa[4][7]." ".$bolsa[4][8]."<br>";
+
+
+?>
+
+</BODY>
+
+</HTML>
